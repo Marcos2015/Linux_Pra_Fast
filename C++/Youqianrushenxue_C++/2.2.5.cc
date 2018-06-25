eace4f154b6457f6bc47273cb9661c985e9daba7
Marcos <herematch@gmail.com>
2018-06-26 06:23:33 +0800

    delete something

---
+++ C++/Youqianrushenxue_C++/2.2.5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	bool bflag;
+	int flag;
+	
+	bflag=true;
+	flag=true;
+	
+	cout << "bflag = " << bflag << endl;
+	cout <<"flag = "<< flag <<endl;
+
+	return 0;
+}
+
+

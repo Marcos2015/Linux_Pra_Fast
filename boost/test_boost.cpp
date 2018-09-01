d2ed0659efd235af14e0eb161996f9fb61ac358b
Marcos <herematch@gmail.com>
2018-09-02 01:41:05 +0800

    solve boost compile

--- boost/test_boost.cpp
+++ boost/test_boost.cpp
@@ -16,3 +16,11 @@ int main(int argc,char** argv)
 }
 
 
+
+//编译命令:g++ -I /usr/local/Celler/boost/1.67.0_1/include/boost/  \
+//             -L /usr/local/Cellar/boost/1.67.0_1/lib \
+//             -lpthread  -lboost_system -lboost_thread-mt  test_boost.cpp
+//             -I 表示需要链接的第三方库头文件的位置
+//             -L 表示需要链接的第三方库的库的位置
+//             -l 指明需要链接的第三方库的名字
+//             boost库的pthread名字叫boost_thread-mt,不是boost_pthread

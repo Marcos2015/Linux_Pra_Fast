f231fde16b16595aed86755f607caca4783f5e31
Marcos <herematch@gmail.com>
2018-10-08 15:53:43 +0800

    delete Thread folder

--- C++/STL/Set/set.cc
+++ C++/STL/Set/set.cc
@@ -6,4 +6,21 @@ using namespace std;
 
 int main()
 {
+    set<int> c;
+
+    c.insert(1);
+    c.insert(2);
+    c.insert(5);
+    c.insert(8);
+    c.insert(9);
+
+
+    cout << *c.lower_bound(4)<<endl;
+    cout << *c.upper_bound(8)<<endl;
+
+
+
+return 0;
+}
+
 

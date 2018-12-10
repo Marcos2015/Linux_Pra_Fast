f88c9b827bcfa9fe990bf79a810cbbd55961389b
Marcos <herematch@gmail.com>
2018-12-10 18:16:37 +0800

    add execle function

--- Linux/APUE/environ.c
+++ Linux/APUE/environ.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-extern char** environ[];
+extern char** environ;
 
 int main()
 {
 	int i=0;
-	while(environ[i]!=0)
+	while(environ[i])
 	{
 		printf("%s\n",environ[i]);
 		i++;

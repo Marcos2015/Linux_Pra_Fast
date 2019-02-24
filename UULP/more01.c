c2a1be967fc854be2e6a553f4d0bb1f6f589612c
feng zhao <zhaofeng@192.168.0.101>
2019-02-24 14:29:42 +0800

    add UULP

---
+++ UULP/more01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define PAGELEN 2
+#define LINELEN 512
+void do_more(FILE *)
+int see_more();
+
+int main(int argc,char** argv)
+{
+	FILE* fp;
+	if(ac== 1)
+	{
+		do_more(stdin);
+	}
+	else
+	{
+		while(--ac)
+		{
+			

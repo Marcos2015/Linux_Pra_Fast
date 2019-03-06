8e815035432ee9ed587e1cf270bb41eea95910a0
Thomas <herematch@gmail.com>
2019-03-06 21:44:15 +0800

    add 8.18

---
+++ Shell/8th/8-18.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+echo "Please input the num:"
+read num
+
+signal=0
+
+while [[ "$signal" != 1 ]]
+do 
+	if [ "$num" -lt 4 ]
+	then 
+		echo "Too small.Try again!"
+		read num
+	else [ "$num" -gt 4 ]
+	then 
+		echo "To high.Try again"
+		read num
+	else
+		signal=1
+		echo "Congratulation ,you are right!"
+	fi
+done
+

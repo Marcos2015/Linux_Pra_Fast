d5003ac76f4104deaf427c7e8d2c5b7ddefe03aa
feng zhao <zhaofeng@192.168.0.100>
2019-03-06 21:15:56 +0800

    add 8-17

--- Shell/8th/8-17.sh
+++ Shell/8th/8-17.sh
@@ -8,7 +8,7 @@ factorial=1
 while [ "$num" -gt 0 ]
 do
 	let "factorial=factorial*num" # 先把最高阶乘乘以1，保存在factorial,然后num--，再一次乘以num,继续不断这样循环下去，达到目的.
-	num--
+	((num--))   # 不可以写成: num--(书上的写法) ，否则报错，可能bash语法不一样
 done
 
 echo "The factorial is $factorial"
